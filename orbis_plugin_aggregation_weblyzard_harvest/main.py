225fc0020d77a869da5c80ebd14285419f467ddb
Odoni Fabian 1 <fabian.odoni@fhgr.ch>
2020-07-23 00:55:17 +0200

    Fix: Will no more crash if service returns nothing by returning nothing and not trying to access the text attribute of that returned nothing.

--- orbis_plugin_aggregation_weblyzard_harvest/main.py
+++ orbis_plugin_aggregation_weblyzard_harvest/main.py
@@ -13,42 +13,49 @@ logger = logging.getLogger("Weblyzard_Harvest")
 class Main(AggregationBaseClass):
 
     def query(self, item):
-        service_url = 'http://localhost:5000/extract_from_html'
+        """
 
-        print(item['url'])
+        Args:
+            item (dict):
+
+        Returns:
+            dict:
+
+        """
+        service_url = 'http://localhost:5000/extract_from_html'
         data = {'url': item['url'], 'html': item['corpus'], 'text': item['corpus_modified']}
 
         try:
             response = requests.post(service_url, json=data)
         except Exception as exception:
             print(f"Query failed: {exception}")
-            response = None
+            return None
 
         response_dict = json.loads(response.text)
-        # print(f"Response: {response_dict['entities']}")
         return response_dict
 
     def map_entities(self, response, item):
+        """
+
+        Args:
+            response (dict):
+            item (dict):
+
+        Returns:
+            list:
+
+        """
         file_entities = []
 
         if not response:
             return None
+
         current_index = 0
         for entity, entity_data in response["entities"].items():
             for doc in entity_data:
-                print(doc)
-                """
-                'doc_id': doc_id,
-                    'type': item,
-                    'surface_form'
-                result['  '][doc_id]
-                """
-                # print(f"\n{doc['type']}")
-                # print(f"Surface_form {doc['surface_form']}")
-                # print(doc['type'])
-
                 doc['key'] = doc['doc_id']
                 doc["entity_type"] = doc['type']
+
                 if doc.get("surface_form"):
                     doc["surfaceForm"] = doc['surface_form']
                     if doc.get("start") and doc.get("end"):
@@ -56,9 +63,12 @@ class Main(AggregationBaseClass):
                         doc["document_end"] = doc['end']
                         end_index = doc['end']
                     else:
-                        start_index_, end_index = get_start_end_position(doc['surface_form'], item['corpus_modified'],
-                                                                         current_index)
-                        doc["document_start"] = start_index_
+                        start_index, end_index = get_start_end_position(
+                            doc['surface_form'],
+                            item['corpus_modified'],
+                            current_index
+                        )
+                        doc["document_start"] = start_index
                         doc["document_end"] = end_index
                     file_entities.append(doc)
                     if end_index > current_index:
